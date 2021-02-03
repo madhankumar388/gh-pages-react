a9a822e7dfd23920cf22bca4b94cb4232da96103
Madhan kumar <madhankumar388@gmail.com>
2021-02-03 11:55:36 -0600

    added custom message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Madhans Github page running using react
         </p>
         <a
           className="App-link"
